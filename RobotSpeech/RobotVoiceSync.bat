e8a0adfed01dcf64bd1f495bf9b861ca55249041
freder1ckashby3 <47575654+freder1ckashby3@users.noreply.github.com>
2019-02-18 22:01:16 -0500

    Add files via upload
    Optimized for less file transfers

--- RobotSpeech/RobotVoiceSync.bat
+++ RobotSpeech/RobotVoiceSync.bat
@@ -6,8 +6,6 @@ cls
 
 scp -P22 "root@10.16.169.120:/22/speech.txt" "C:\RobotSpeech\speech.txt"
 
-scp -P22 "root@10.16.169.120:/22/speechnumber.txt" "C:\RobotSpeech\Speech Validation\speechnumber.txt"
-
 C:\RobotSpeech\pyttsx-OT-2.py
 
 goto start
--- RobotSpeech/pyttsx-OT-2.py
+++ RobotSpeech/pyttsx-OT-2.py
@@ -3,19 +3,16 @@ from shutil import copyfile
 
 engine = pyttsx3.init()
 
-speech = open('C:\\RobotSpeech\\speech.txt', 'r')
-speechnum = open('C:\\RobotSpeech\\Speech Validation\\speechnumber.txt', 'r')
-old_speechnum = open('C:\\RobotSpeech\\Speech Validation\\old_speechnumber.txt', 'r')
-
-speech_content = speech.read()
-speech_check = int(speechnum.read())
-last_speech = int(old_speechnum.read())
-
-if last_speech == speech_check:
-	print("Speech not yet updated.")
+speech_content = open('C:\\RobotSpeech\\speech.txt', 'r').read()
+last_speech = open('C:\\RobotSpeech\\Speech Validation\\speech.txt', '+r').read()
+closing_speech = open('C:\\RobotSpeech\\Speech Validation\\Close\\speech.txt', 'r').read()
+	
+if str(last_speech) == str(speech_content):
+	print("...")
+	time.sleep(1)
 else:
 	engine.say(speech_content)
 	engine.runAndWait()
 	print("ROBOT: " + speech_content)
-
-copyfile('C:\\RobotSpeech\\Speech Validation\\speechnumber.txt', 'C:\\RobotSpeech\\Speech Validation\\old_speechnumber.txt')
+	copyfile('C:\\RobotSpeech\\speech.txt', 'C:\\RobotSpeech\\Speech Validation\\speech.txt')
+	time.sleep(1)
